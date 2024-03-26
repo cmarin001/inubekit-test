14b888f65446f398bd7bedec9a809524780f4cba
cmarin001 <cmarin6051@gmail.com>
2024-03-26 15:00:10 -0500

    fix(auto): change the auto configuration in  orderder to make the releases automatically

--- auto.config.ts
+++ auto.config.ts
@@ -1,33 +1,15 @@
 import { AutoRc } from "auto";
 
-import { INpmConfig } from "@auto-it/npm";
-import { IAllContributorsPluginOptions } from "@auto-it/all-contributors";
-
-const npmOptions: INpmConfig = {
-  exact: true,
-  canaryScope: "@auto-canary",
-};
-
-const allContributorsOptions: IAllContributorsPluginOptions = {
-  types: {
-    plugin: "**/plugin/**/*",
-    code: ["**/src/**/*", "**/package.json", "**/tsconfig.json"],
-  },
-};
-
-/** Auto configuration */
 export default function rc(): AutoRc {
   return {
-    plugins: [
-      "released",
-      ["npm", npmOptions],
-      ["all-contributors", allContributorsOptions],
-    ],
+    plugins: ["released", "npm", "magic-zero"],
     labels: [
       {
         name: "blog-post",
         changelogTitle: "📚 Blog Post",
         releaseType: "none",
+        major: "major",
+        minor: "minor",
       },
     ],
   };
